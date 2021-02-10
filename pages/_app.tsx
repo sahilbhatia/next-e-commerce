0f26ea4a4b54131793294759a8c6f9175ee1b8c1
Mandar Athavale <mandarathavale29595@gmail.com>
2021-02-10 14:17:46 +0530

    Remove redux setup.

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import type { AppProps } from 'next/app'
-import { wrapper } from '../store'
-
-function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
-
-export default wrapper.withRedux(App)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,5 @@
-import { useDispatch } from 'react-redux'
-import { wrapper } from '../store'
-import { setCount } from '../actions'
-
 const Index = () => {
-  const dispatch = useDispatch()
-
-  // Example for basic redux store updation. Remove later.
-  const buttonClicked = () => {
-    dispatch(setCount())
-  }
-
-  return (
-    <div>
-      <button onClick={buttonClicked}>Click Me</button>
-    </div>
-  )
+  return <div>Hi Welcome to e-commerce app</div>
 }
-
-export const getStaticProps = wrapper.getStaticProps(async ({ store }) => {})
 
 export default Index

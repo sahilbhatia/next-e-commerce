8fc27942f2b6866562e8998281c03810640c57cb
Mandar Athavale <mandarathavale29595@gmail.com>
2021-02-16 10:42:47 +0530

    Use classnames module for conditionally applying styles

--- components/productCard.jsx
+++ components/productCard.jsx
@@ -1,3 +1,5 @@
+import classnames from 'classnames'
+
 import QuantityChanger from './quantityChanger'
 import ProductImage from './productImage'
 import ProductCategory from './productCategory'
@@ -24,30 +26,45 @@ const ProductCard = (props) => {
 
   return (
     <div
-      className={
-        'rounded bg-gray-200 overflow-ellipsis m-4 ' +
-        (landscape ? 'flex w-4/5 ' : 'max-w-xs ')
-      }
+      className={classnames(
+        'rounded',
+        'bg-gray-200',
+        'overflow-ellipsis',
+        'm-4',
+        {
+          'flex w-4/5': landscape,
+          'max-w-xs': !landscape
+        }
+      )}
     >
-      <div className={(landscape ? 'w-40 ' : 'w-auto ') + 'px-4 py-4'}>
+      <div
+        className={classnames(
+          { 'w-40': landscape, 'w-auto': !landscape },
+          'px-4',
+          'py-4'
+        )}
+      >
         <ProductImage thumbnailUrl={product.thumbnailUrl} />
       </div>
       <div
-        className={
-          (landscape ? 'flex mx-4 my-4 flex items-center w-3/8' : 'block ') +
-          'px-4 py-4'
-        }
+        className={classnames(
+          {
+            'flex mx-4 my-4 flex items-center w-3/8': landscape,
+            block: !landscape
+          },
+          'px-4',
+          'py-4'
+        )}
       >
         <ProductName name={product.name} />
         <ProductCategory category={product.category} />
         <ProductPrice price={product.price} />
       </div>
       <div
-        className={
-          landscape
-            ? 'flex flex-col items-end justify-center w-5/12 mx-4 my-4'
-            : 'px-4 py-4'
-        }
+        className={classnames({
+          'flex flex-col items-end justify-center w-5/12 mx-4 my-4': landscape,
+          'px-4 py-4': !landscape
+        })}
       >
         {!productInCart && (
           <AddToCartButton
